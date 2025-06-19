d4484fcd472b76a56be211d3c51e6551d91a2b01
Meenaa Kannan <meenaa.kannan1@outlook.com>
2025-06-19 12:26:23 -0700

    Adding Checkout Page

--- .features-gen/tests/Features/SubscriptionPage.feature.spec.js
+++ .features-gen/tests/Features/SubscriptionPage.feature.spec.js
@@ -21,6 +21,12 @@ test.describe('Subscription page for unauthenticated users', () => {
     await Then('The user should see a pop-up toast notification window with login button to sign-in'); 
   });
 
+  test('Verifying that a signed-up user can subscribe to Premium', async ({ Given, page, When, Then }) => { 
+    await Given('The user signs in using a valid username and password from the Signin window', null, { page }); 
+    await When('The user attempts to upgrade to a Premium subscription'); 
+    await Then('The user should be redirected to the checkout page'); 
+  });
+
 });
 
 // == technical section ==
@@ -40,4 +46,5 @@ const bddFileData = [ // bdd-data-start
   {"pwTestLine":6,"pickleLine":5,"tags":[],"steps":[{"pwStepLine":7,"gherkinStepLine":6,"keywordType":"Context","textWithKeyword":"Given The user is on the Subscription Page","stepMatchArguments":[]},{"pwStepLine":8,"gherkinStepLine":7,"keywordType":"Action","textWithKeyword":"When The user clicks Get Started Free button","stepMatchArguments":[]},{"pwStepLine":9,"gherkinStepLine":8,"keywordType":"Outcome","textWithKeyword":"Then The user should see a pop-up window prompting sign-in","stepMatchArguments":[]}]},
   {"pwTestLine":12,"pickleLine":10,"tags":[],"steps":[{"pwStepLine":13,"gherkinStepLine":11,"keywordType":"Context","textWithKeyword":"Given The user is on the Subscription Page","stepMatchArguments":[]},{"pwStepLine":14,"gherkinStepLine":12,"keywordType":"Action","textWithKeyword":"When The user clicks Subscribe now button","stepMatchArguments":[]},{"pwStepLine":15,"gherkinStepLine":13,"keywordType":"Outcome","textWithKeyword":"Then The user should get the pop-up toast notification window with login button to sign-in","stepMatchArguments":[]}]},
   {"pwTestLine":18,"pickleLine":15,"tags":[],"steps":[{"pwStepLine":19,"gherkinStepLine":16,"keywordType":"Context","textWithKeyword":"Given The user is on the Subscription Page","stepMatchArguments":[]},{"pwStepLine":20,"gherkinStepLine":17,"keywordType":"Action","textWithKeyword":"When The user clicks Subscribe now button","stepMatchArguments":[]},{"pwStepLine":21,"gherkinStepLine":18,"keywordType":"Outcome","textWithKeyword":"Then The user should see a pop-up toast notification window with login button to sign-in","stepMatchArguments":[]}]},
+  {"pwTestLine":24,"pickleLine":20,"tags":[],"steps":[{"pwStepLine":25,"gherkinStepLine":21,"keywordType":"Context","textWithKeyword":"Given The user signs in using a valid username and password from the Signin window","stepMatchArguments":[]},{"pwStepLine":26,"gherkinStepLine":22,"keywordType":"Action","textWithKeyword":"When The user attempts to upgrade to a Premium subscription","stepMatchArguments":[]},{"pwStepLine":27,"gherkinStepLine":23,"keywordType":"Outcome","textWithKeyword":"Then The user should be redirected to the checkout page","stepMatchArguments":[]}]},
 ]; // bdd-data-end
