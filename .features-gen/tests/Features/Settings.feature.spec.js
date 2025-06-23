50a969a49b6bdf8e5236466b30bb3dd74e1cea8b
Reshmi <r.reshmi87@gmail.com>
2025-06-22 18:26:42 -0700

    Previous Assessment spec file

--- .features-gen/tests/Features/Settings.feature.spec.js
+++ .features-gen/tests/Features/Settings.feature.spec.js
@@ -37,6 +37,12 @@ test.describe('Settings Page validation', () => {
     await Then('All the information are displayed properly for the user signed in through Google Account', null, { page }); 
   });
 
+  test('Validate Manan link is functional from the Settings page', async ({ Given, page, When, Then }) => { 
+    await Given('The user is in the Settings Page', null, { page }); 
+    await When('The user clicks on the link Manan at the top in Settings page', null, { page }); 
+    await Then('Self Assessment page is displayed when the clicked on Manan from Settings Page', null, { page }); 
+  });
+
 });
 
 // == technical section ==
@@ -58,4 +64,5 @@ const bddFileData = [ // bdd-data-start
   {"pwTestLine":22,"pickleLine":16,"tags":[],"steps":[{"pwStepLine":7,"gherkinStepLine":4,"keywordType":"Context","textWithKeyword":"Given The user launched the Manan application and logged in successfully","isBg":true,"stepMatchArguments":[]},{"pwStepLine":23,"gherkinStepLine":17,"keywordType":"Context","textWithKeyword":"Given The user is in the Dashboard page","stepMatchArguments":[]},{"pwStepLine":24,"gherkinStepLine":18,"keywordType":"Action","textWithKeyword":"When The user clicks on the Update settings link from Account settings block","stepMatchArguments":[]},{"pwStepLine":25,"gherkinStepLine":19,"keywordType":"Outcome","textWithKeyword":"Then Medical Research block is displayed in settings page","stepMatchArguments":[]}]},
   {"pwTestLine":28,"pickleLine":21,"tags":[],"steps":[{"pwStepLine":7,"gherkinStepLine":4,"keywordType":"Context","textWithKeyword":"Given The user launched the Manan application and logged in successfully","isBg":true,"stepMatchArguments":[]},{"pwStepLine":29,"gherkinStepLine":22,"keywordType":"Context","textWithKeyword":"Given The user is in the Settings page","stepMatchArguments":[]},{"pwStepLine":30,"gherkinStepLine":23,"keywordType":"Action","textWithKeyword":"When The user make changes to the Email and App Notifications and clicks on save changes","stepMatchArguments":[]},{"pwStepLine":31,"gherkinStepLine":24,"keywordType":"Outcome","textWithKeyword":"Then The changes are saved and the user is able to see the changes whenever he comes back to the page.","stepMatchArguments":[]}]},
   {"pwTestLine":34,"pickleLine":26,"tags":[],"steps":[{"pwStepLine":7,"gherkinStepLine":4,"keywordType":"Context","textWithKeyword":"Given The user launched the Manan application and logged in successfully","isBg":true,"stepMatchArguments":[]},{"pwStepLine":35,"gherkinStepLine":27,"keywordType":"Context","textWithKeyword":"Given The user logged in as Google user","stepMatchArguments":[]},{"pwStepLine":36,"gherkinStepLine":28,"keywordType":"Action","textWithKeyword":"When User Navigates to the Settings page","stepMatchArguments":[]},{"pwStepLine":37,"gherkinStepLine":29,"keywordType":"Outcome","textWithKeyword":"Then All the information are displayed properly for the user signed in through Google Account","stepMatchArguments":[]}]},
+  {"pwTestLine":40,"pickleLine":31,"tags":[],"steps":[{"pwStepLine":7,"gherkinStepLine":4,"keywordType":"Context","textWithKeyword":"Given The user launched the Manan application and logged in successfully","isBg":true,"stepMatchArguments":[]},{"pwStepLine":41,"gherkinStepLine":32,"keywordType":"Context","textWithKeyword":"Given The user is in the Settings Page","stepMatchArguments":[]},{"pwStepLine":42,"gherkinStepLine":33,"keywordType":"Action","textWithKeyword":"When The user clicks on the link Manan at the top in Settings page","stepMatchArguments":[]},{"pwStepLine":43,"gherkinStepLine":34,"keywordType":"Outcome","textWithKeyword":"Then Self Assessment page is displayed when the clicked on Manan from Settings Page","stepMatchArguments":[]}]},
 ]; // bdd-data-end
